860191b4a654e520286c17e7e2321c59db09f29e
E-Kat101 <74022900+E-Kat101@users.noreply.github.com>
2023-04-19 22:34:24 -0400

    Update intro_practice.py

--- 6.1 main() function/intro_practice.py
+++ 6.1 main() function/intro_practice.py
@@ -1,3 +1,4 @@
+# Hello, Name!
 def main():
     name = input("Enter your name: ")
 
@@ -7,4 +8,38 @@ def main():
     print(f"Hello, {name}!")
 
 if __name__ == "__main__":
+    main()
+
+   # Area Calculator
+def main():
+    print("Welcome to the awesome area calculator!")
+    print()
+    
+    length = int(input("Enter a length: "))
+    width = int(input("Enter a width: "))
+    print()
+    
+    area = length * width
+    
+    print(f"The area is {area} units squared")
+
+if __name__ == "__main__":
+    main()
+    
+# Area Calculator greet() Function
+def main():
+    length = int(input("Enter a length: "))
+    width = int(input("Enter a width: "))
+    print()
+    
+    area = length * width
+    
+    print(f"The area is {area} units squared")
+
+def greet():
+    print("Welcome to the awesome area calculator!")
+    print()
+
+if __name__ == "__main__":
+    greet()
     main()
